7e0d5378ef76b9edaed184eab98feb4f104976b2
Jakub Lužný <limoto94@gmail.com>
2011-06-24 20:39:19 +0200

    Přidán dummy engine jako vzorový

---
+++ engines/dummy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+__author__ = "Jakub Lužný"
+__desc__ = "Dummy"
+__url__ = r"http://www\.dummy\.net/.*"
+
+import re
+from urllib.request import urlopen
+
+class DummyEngine:
+
+    def __init__(self, url):
+        self.page = urlopen(url).read().decode('utf-8')
+        
+    def qualities(self):
+        return [ ('high', 'Vysoká'), ('low', 'Nízká') ]
+        
+    def movies(self):
+        return [ ('0', 'Jedno video'), ('1', 'Druhý video') ]
+
+    def download(self, quality, movie):
+        return ('rtmp', 'output.flv', {'url' : 'rtmp://server.net:port/appname/playpath', 'playpath' : 'opravdu/divnej/playpath', 'app' : 'opravdu/divná/aplikace', 'rtmpdump_args' : '--live'} )
+        return ('error', 'Slunci došel vodík')
